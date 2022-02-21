0d6d8786559c3a7925731502db990dc8dd4013dc
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-21 13:21:35 +0100

    setup navbar with functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,47 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
+import './styles/NavBar.css';
 
-const NavBar = () => <h3>I am the navbar, waiting for links</h3>;
+const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'BOOKS',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'CATEGORIES',
+    },
+  ];
+  return (
+    <>
+      <nav className="bookNav">
+        <ul>
+          <li className="nav-name">Bookstore CMS</li>
+          {links.map(({ id, path, text }) => (
+            <li key={id}>
+              <NavLink
+                to={path}
+                className="navLink"
+                style={({ isActive }) => ({
+                  textDecoration: 'none',
+                  opacity: isActive ? '1' : '0.5',
+                })}
+              >
+                {text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <div className="avatar">
+          <ImUser />
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default NavBar;
