d60b30fade72d050f7b76c869a04b89bcf0a91d1
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-21 15:18:31 +0100

    adding features to list items

--- src/components/BooksItem.js
+++ src/components/BooksItem.js
@@ -3,8 +3,38 @@ import styles from './styles/BooksItem.module.css';
 
 const BooksItem = () => (
   <>
-    <li className={styles.listItem}>Harry Potter</li>
-    <li className={styles.listItem}>Pirates of the carrebian</li>
+    <li className={styles.listItem}>
+      <ul className={styles.itemsList}>
+        <li>
+          {' '}
+          <p className={styles.bookTopic}>Action</p>
+          <h2>The Hunger Games</h2>
+          <span className={styles.author}>Suzanne Collins</span>
+          <ul
+            style={{
+              marginTop: '55px',
+              listStyle: 'none',
+              display: 'flex',
+              flexDirection: 'row',
+              cursor: 'pointer',
+            }}
+          >
+            <li>Comments</li>
+            <li>Remove</li>
+            <li>Edit</li>
+          </ul>
+        </li>
+        <li>
+          <div>circle</div>
+          <div>64% Completed</div>
+        </li>
+        <li style={{ marginLeft: '50px', borderLeft: 'solid 2px var(--border-color)' }}>
+          <p className={styles.chapter}>CURRENT CHAPTER</p>
+          <p className={styles.lesson}>Chapter 17</p>
+          <button type="button">UPDATE PROGRESS</button>
+        </li>
+      </ul>
+    </li>
   </>
 );
 
